7e6afaec76621630fd4e1377908666a721b55dc5
h4xxel <isaksson.axel@telia.com>
2015-04-14 17:47:49 +0200

    c runtime from chipkit

---
+++ runtime/crtprep/crti.s
@@ -0,0 +1,4 @@
+/* Dude, where's my crti? */
+/* where's your crti, dude? */
+/* DUDE, where's my crti? */
+/* where's your crti, dude? */
---
+++ runtime/crtprep/crtn.s
@@ -0,0 +1,1 @@
+/* No crtn was needed when building this toolchain. Well, not one containing anything anyway. */
