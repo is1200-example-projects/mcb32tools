214cdf44fc0618484ac0d4fe33a46665b6d89d01
Steven Arnow <s@rdw.se>
2015-04-20 17:40:20 +0200

    Reworked environment launch on Mac OS

--- os-specific/mac/pic32-toolchain-launch.c
+++ os-specific/mac/pic32-toolchain-launch.c
@@ -9,11 +9,15 @@ int main(int argc, char **argv) {
 	struct stat s;
 	char message[4096];
 
-	snprintf(message, 4096, "echo 'display dialog \"You must install this toolchain directly under /Applications with the filename %s\"' | osascript", basename(MAC_APP_PATH));
+	snprintf(message, 4096, "echo 'display dialog \"The PIC32 toolchain must be installed directly in the applications directory, not in a subdirectory, and with the file name %s\" with title \"Error\" with icon stop buttons {\"OK\"}' | osascript", basename(MAC_APP_PATH));
 	if (stat("$PREFIX_DATA_ROOT/Resources/Toolchain", &s) < 0 || !S_ISDIR(s.st_mode)) {
 		system(message);
 		return 1;
 	}
 
-	return system("open -a Terminal.app $PREFIX_DATA_ROOT/MacOS/launchterm");
+	return system("osascript << EOF
+			tell application \"Terminal\"
+				do script \". '$PREFIX_DATA_ROOT/Resources/Toolchain/environment'\"
+			end tell
+			EOF");
 }
