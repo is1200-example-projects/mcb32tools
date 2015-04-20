ec5ca78662ec1e040c813ee1622a24aeb26e1689
Steven Arnow <s@rdw.se>
2015-04-20 15:54:30 +0200

    Fixed pic32-toolchain-launch.c

--- os-specific/mac/pic32-toolchain-launch.c
+++ os-specific/mac/pic32-toolchain-launch.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -8,8 +9,8 @@ int main(int argc, char **argv) {
 	struct stat s;
 	char message[4096];
 
-	snprintf(message, "echo 'display dialog \"You must install this toolchain directly under /Applications with the filename %s\"' | osascript", basename(PREFIX_APP_PATH));
-	if (stat("$PREFIX_DATA_ROOT/Resources/Toolchain", &s) < 0 || !S_ISDIR(s)) {
+	snprintf(message, 4096, "echo 'display dialog \"You must install this toolchain directly under /Applications with the filename %s\"' | osascript", basename(PREFIX_APP_PATH));
+	if (stat("$PREFIX_DATA_ROOT/Resources/Toolchain", &s) < 0 || !S_ISDIR(s.st_mode)) {
 		system(message);
 		return 1;
 	}
