19b68241ebbbec8ec0604ecf3e73f880e424c676
Steven Arnow <s@rdw.se>
2015-04-20 16:06:08 +0200

    Hide failed wget, fix build for mac

--- os-specific/mac/pic32-toolchain-launch.c
+++ os-specific/mac/pic32-toolchain-launch.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 	struct stat s;
 	char message[4096];
 
-	snprintf(message, 4096, "echo 'display dialog \"You must install this toolchain directly under /Applications with the filename %s\"' | osascript", basename(PREFIX_APP_PATH));
+	snprintf(message, 4096, "echo 'display dialog \"You must install this toolchain directly under /Applications with the filename %s\"' | osascript", basename(MAC_APP_PATH));
 	if (stat("$PREFIX_DATA_ROOT/Resources/Toolchain", &s) < 0 || !S_ISDIR(s.st_mode)) {
 		system(message);
 		return 1;
