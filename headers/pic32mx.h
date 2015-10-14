982cc6676a9b7adf0a9c46b2f1fff845aa9ea4a2
h4xxel <isaksson.axel@telia.com>
2015-10-14 14:50:00 +0200

    added i2c registers to pic32mx.h

--- headers/pic32mx.h
+++ headers/pic32mx.h
@@ -2,6 +2,7 @@
  * Hardware register defines for all Microchip PIC32MX microcontrollers.
  *
  * Copyright (C) 2010 Serge Vakulenko, <serge@vak.ru>
+ * Modified 2015 by Axel Isaksson - Added I2C registers
  *
  * Permission to use, copy, modify, and distribute this software
  * and its documentation for any purpose and without fee is hereby
@@ -836,6 +837,98 @@
 #define PIC32_SPISTAT_SPIRBE    0x00000020      /* Receive buffer is empty */
 #define PIC32_SPISTAT_SPIROV	0x00000040      /* Receive overflow flag */
 #define PIC32_SPISTAT_SPIBUSY	0x00000800      /* SPI is busy */
+
+
+/*--------------------------------------
+ * I2C Registers.
+ */
+#define I2C1CON		PIC32_R (0x5000)
+#define I2C1CONCLR	PIC32_R (0x5004)
+#define I2C1CONSET	PIC32_R (0x5008)
+#define I2C1CONINV	PIC32_R (0x500c)
+#define I2C1STAT	PIC32_R (0x5010)
+#define I2C1STATCLR	PIC32_R (0x5014)
+#define I2C1STATSET	PIC32_R (0x5018)
+#define I2C1STATINV	PIC32_R (0x501c)
+#define I2C1ADD		PIC32_R (0x5020)
+#define I2C1ADDCLR	PIC32_R (0x5024)
+#define I2C1ADDSET	PIC32_R (0x5028)
+#define I2C1ADDINV	PIC32_R (0x502c)
+#define I2C1MSK		PIC32_R (0x5030)
+#define I2C1MSKCLR	PIC32_R (0x5034)
+#define I2C1MSKSET	PIC32_R (0x5038)
+#define I2C1MSKINV	PIC32_R (0x503c)
+#define I2C1BRG		PIC32_R (0x5040)
+#define I2C1BRGCLR	PIC32_R (0x5044)
+#define I2C1BRGSET	PIC32_R (0x5048)
+#define I2C1BRGINV	PIC32_R (0x504c)
+#define I2C1TRN		PIC32_R (0x5050)
+#define I2C1TRNCLR	PIC32_R (0x5054)
+#define I2C1TRNSET	PIC32_R (0x5058)
+#define I2C1TRNINV	PIC32_R (0x505c)
+#define I2C1RCV		PIC32_R (0x5060)
+
+#define I2C2CON		PIC32_R (0x5200)
+#define I2C2CONCLR	PIC32_R (0x5204)
+#define I2C2CONSET	PIC32_R (0x5208)
+#define I2C2CONINV	PIC32_R (0x520c)
+#define I2C2STAT	PIC32_R (0x5210)
+#define I2C2STATCLR	PIC32_R (0x5214)
+#define I2C2STATSET	PIC32_R (0x5218)
+#define I2C2STATINV	PIC32_R (0x521c)
+#define I2C2ADD		PIC32_R (0x5220)
+#define I2C2ADDCLR	PIC32_R (0x5224)
+#define I2C2ADDSET	PIC32_R (0x5228)
+#define I2C2ADDINV	PIC32_R (0x522c)
+#define I2C2MSK		PIC32_R (0x5230)
+#define I2C2MSKCLR	PIC32_R (0x5234)
+#define I2C2MSKSET	PIC32_R (0x5238)
+#define I2C2MSKINV	PIC32_R (0x523c)
+#define I2C2BRG		PIC32_R (0x5240)
+#define I2C2BRGCLR	PIC32_R (0x5244)
+#define I2C2BRGSET	PIC32_R (0x5248)
+#define I2C2BRGINV	PIC32_R (0x524c)
+#define I2C2TRN		PIC32_R (0x5250)
+#define I2C2TRNCLR	PIC32_R (0x5254)
+#define I2C2TRNSET	PIC32_R (0x5258)
+#define I2C2TRNINV	PIC32_R (0x525c)
+#define I2C2RCV		PIC32_R (0x5260)
+
+/*
+ * I2C Control register
+ */
+#define PIC32_I2CCON_SEN	0x00000001
+#define PIC32_I2CCON_RSEN	0x00000002
+#define PIC32_I2CCON_PEN	0x00000004
+#define PIC32_I2CCON_RCEN	0x00000008
+#define PIC32_I2CCON_ACKEN	0x00000010
+#define PIC32_I2CCON_ACKDT	0x00000020
+#define PIC32_I2CCON_STREN	0x00000040
+#define PIC32_I2CCON_GCEN	0x00000080
+#define PIC32_I2CCON_SMEN	0x00000100
+#define PIC32_I2CCON_DISSLW	0x00000200
+#define PIC32_I2CCON_A10M	0x00000400
+#define PIC32_I2CCON_STRICT	0x00000800
+#define PIC32_I2CCON_SLCREL	0x00001000
+#define PIC32_I2CCON_SIDL	0x00002000
+#define PIC32_I2CCON_ON		0x00008000
+
+/*
+ * I2C Status register
+ */
+#define PIC32_I2CSTAT_TBF	0x00000001
+#define PIC32_I2CSTAT_RBF	0x00000002
+#define PIC32_I2CSTAT_RW	0x00000004
+#define PIC32_I2CSTAT_S		0x00000008
+#define PIC32_I2CSTAT_P		0x00000010
+#define PIC32_I2CSTAT_DA	0x00000020
+#define PIC32_I2CSTAT_I2COV	0x00000040
+#define PIC32_I2CSTAT_IWCOL	0x00000080
+#define PIC32_I2CSTAT_ADD10	0x00000100
+#define PIC32_I2CSTAT_GCSTAT	0x00000200
+#define PIC32_I2CSTAT_BCL	0x00000400
+#define PIC32_I2CSTAT_TRSTAT	0x00004000
+#define PIC32_I2CSTAT_ACKSTAT	0x00008000
 
 /*--------------------------------------
  * DMA controller registers.
